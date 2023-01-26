3be076fae90d869e3f6f020c7a46e490583df884
Bharat <baru.hegde@gmail.com>
2023-01-26 17:52:00 +0530

    Add react router

---
+++ 02_lesson_starter/src/features/posts/SinglePostPage.js
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+import { useParams } from "react-router";
+
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButton from "./ReactionButtons";
+
+const SinglePostPage = () => {
+  const { postId } = useParams();
+
+  const post = useSelector((state) => selectPostById(state, Number(postId)));
+
+  if (!post) {
+    return (
+      <section>
+        <h2>Post Not Found!</h2>
+      </section>
+    );
+  }
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timeStamp={post.date} />
+        <ReactionButton post={post} />
+      </p>
+    </article>
+  );
+};
+
+export default SinglePostPage;
